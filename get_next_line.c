015b4b8faec15f523ebbeefde7fd9d5e7898ae9a
Jere Florent <jflorent@student.21-school.ru>
2019-09-13 10:55:17 +0300

    almost not temp

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jflorent <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 13:29:20 by jflorent          #+#    #+#             */
-/*   Updated: 2019/09/13 08:03:08 by jflorent         ###   ########.fr       */
+/*   Updated: 2019/09/13 10:52:51 by jflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,29 @@
 ** Search node with coherent fd or create new
 */
 
-static t_rfile		*search_node(t_rfile *list, int fd)
+static t_rfile		*search_node(t_rfile **list, int fd)
 {
 	t_rfile			*new;
 	t_rfile			*temp;
+	t_rfile			*head;
 
-	temp = list;
-	while (list)
+	temp = *list;
+	head = *list;
+	while (temp)
 	{
-		if (list->cfd == fd)
-			return (list);
-		list = list->next;
+		if (temp->cfd == fd)
+			return (temp);
+		temp = temp->next;
 	}
-	if (!list)
+	if (!temp)
 	{
 		new = (t_rfile*)malloc(sizeof(t_rfile));
 		if (!new)
 			return (0);
 		new->cfd = fd;
-		new->next = temp;
+		new->next = head;
 		new->content = 0;
+		*list = new;
 		return (new);
 	}
 	return (0);
@@ -48,25 +51,25 @@ static t_rfile		*search_node(t_rfile *list, int fd)
 static int			get_line(t_rfile *curr, char **line)
 {
 	size_t				i;
-	char				*new;
 	char				*new_content;
 	size_t				n;
 
 	i = 0;
-	if (!(curr->content))
+	if (!(curr->content) || !((curr->content)[i]))
 		return (0);
 	while ((curr->content)[i] != '\n' && (curr->content)[i])
 		i++;
-	new = ft_strnew(i + 1);
-	if (!new)
+	*line = ft_strnew(i + 1);
+	if (!(*line))
 		return (-1);
-	ft_strncpy(new, curr->content, i);
-	*line = new;
+	ft_strncpy(*line, curr->content, i);
+	(*line)[i] = '\0';
 	if ((n = ft_strlen(curr->content) - i - 1))
 	{
 		new_content = ft_strdup(&((curr->content)[i + 1]));
 		if (!new_content)
 			return (-1);
+		free(curr->content);
 		curr->content = new_content;
 	}
 	else
@@ -103,6 +106,39 @@ static int			string_create(t_rfile *node, size_t n, char *buff)
 }
 
 /*
+** Clears empty nodes
+*/
+
+static void			node_clear(t_rfile **head)
+{
+	t_rfile			*temp;
+	t_rfile			*prev;
+
+	prev = 0;
+	temp = *head;
+	while (temp)
+	{
+		if (!prev && !(temp->content))
+		{
+			*head = temp->next;
+			free(temp);
+			temp = *head;
+		}
+		else if (prev && !(temp->content))
+		{
+			prev->next = temp->next;
+			free(temp);
+			temp = prev->next;
+		}
+		else
+		{
+			prev = temp;
+			temp = temp->next;
+		}
+	}
+}
+
+/*
 ** Call searching node, filling node and cleaning node
 */
 
@@ -110,19 +146,23 @@ int					get_next_line(const int fd, char **line)
 {
 	static t_rfile	*node;
 	char			buff[BUFF_SIZE + 1];
-	size_t			n;
+	int				n;
+	t_rfile			*curr;
 
 	n = 0;
-	if (!line || !(node = search_node(node, fd)))
+	if (!line || !(curr = search_node(&node, fd)) || fd < 0)
 		return (-1);
 	while ((n = read(fd, buff, BUFF_SIZE)))
 	{
+		if (n == -1)
+			return (-1);
 		buff[n] = '\0';
-		if (!string_create(node, n, buff))
+		if (!string_create(curr, n, buff))
 			return (-1);
 		if (ft_strchr(buff, '\n'))
 			break ;
 	}
-	n = get_line(node, line);
+	n = get_line(curr, line);
+	node_clear(&node);
 	return (n);
 }
--- test1.c
+++ test1.c
@@ -9,10 +9,30 @@ int			main()
 {
 	char	*line;
 	int		fda;
+	int		fdb;
+	int		fdc;
+	int		i;
 
+	i = 0;
 	line = (char*)malloc(sizeof(char) * 200);
 	fda = open("get_next_line.c", O_RDONLY);
+	fdb = open("new.txt", O_RDONLY);
+	fdc = open("new1.txt", O_RDONLY);
+	if (fdc == -1)
+		return (1);
+	for (i = 0; i < 10; i++)
+	{
+		get_next_line(fda, &line);
+		printf("%s\n", line);
+	}
+	while (get_next_line(fdb, &line) == 1)
+		printf("%s\n", line);
 	while (get_next_line(fda, &line) == 1)
 		printf("%s\n", line);
+	while (get_next_line(fdc, &line) == 1)
+		printf("%s\n", line);
+	close(fda);
+	close(fdb);
+	close(fdc);
 	return (0);
 }
